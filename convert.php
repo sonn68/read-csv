0f8b5030cf9b6853af11c52a8a1f441cdf3731a7
son <ngotheson1@gmail.com>
2019-09-20 01:08:07 +0700

    create form upload

--- convert.php
+++ convert.php
@@ -24,30 +24,30 @@
 </div>
 <script type="text/javascript" src="csv2json.js"></script>
 <script>
+  let jsonResult
   $.get("content.php", function(data, status){
-    const json = CSVJSON.csv2json(data, {parseNumbers: true});
-    const arrKey = Object.keys(json[0])
-    arrKey.map(key => document.getElementById("row").innerHTML += `
-        <th>${key}</th>\n
-    `)
-    let column = document.getElementById("column")
-    json.map(obj => {
-      column.innerHTML += `
-        <tr>\n
-      `
-      let stringTd = ''
-      arrKey.map(key => {
-        stringTd += `
-          <td>${obj[key]}</td>\n
-        `
-      })
-      column.innerHTML += stringTd
-      column.innerHTML += `
-        </tr>\n
-      `
-    })
-
+    jsonResult = CSVJSON.csv2json(data, {parseNumbers: true});
   });   
+  const arrKey = Object.keys(jsonResult[0])
+  arrKey.map(key => document.getElementById("row").innerHTML += `
+    <th>${key}</th>\n
+  `)
+  let column = document.getElementById("column")
+  jsonResult.map(obj => {
+    column.innerHTML += `
+      <tr>\n
+    `
+  let stringTd = ''
+  arrKey.map(key => {
+    stringTd += `
+      <td>${obj[key]}</td>\n
+    `
+  })
+  column.innerHTML += stringTd
+  column.innerHTML += `
+    </tr>\n
+  `     
+  })
 </script>
 </body>
 </html>
