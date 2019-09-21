470c9629919dc08a595eca8e957c5fa8a3190474
son <ngotheson1@gmail.com>
2019-09-21 22:30:22 +0700

    test ok

--- convert.js
+++ convert.js
@@ -12,24 +12,40 @@ const file3 = document.getElementById("dom-target3");
 const contentFile3 = file3.textContent;
 
 let column = document.getElementById("column")
-$( "#column" ).append( `
-  <td>${country[1].file1}</td>\n
-`)
-setContentFile1(contentFile1, country[0].file1, country[1].file1, country[2].file1)
+// $( "#column" ).append( `
+//   <td>${country[1].file1}</td>\n
+// `)
+setContentFile1(contentFile1, country[0].file1, country[1].file1, country[2].file1,null,appendCountry)
 setContentFile1(contentFile2, country[0].file2, country[1].file2, country[2].file2)
-setContentFile1(contentFile3, country[0].file3, country[1].file3, country[2].file3)
-$( "#column" ).append( `
-  <tr></tr>
-`)
-setContentFile1(contentFile1, country[0].file1, country[1].file1, country[2].file1)
+setContentFile1(contentFile3, country[0].file3, country[1].file3, country[2].file3, appendTr)
+
+setContentFile1(contentFile1, country[0].file1, country[1].file1, country[2].file1,null,appendCountry)
 setContentFile1(contentFile2, country[0].file2, country[1].file2, country[2].file2)
 setContentFile1(contentFile3, country[0].file3, country[1].file3, country[2].file3)
 
-function setContentFile1 (contentFile, fieldCountry, country, field) {
+function setContentFile1 (contentFile, fieldCountry, country, field, appendTr, appendCountry) {
   $.get(contentFile, function(data, status){
+    if(appendCountry) {
+      console.log(country)
+      appendCountry(country)
+    }
     const json = CSVJSON.csv2json(data, {parseNumbers: true});
     const obj = json.filter(obj => obj[fieldCountry] == country)
     const string = `<td>${obj[0][field]}</td>\n`
     $( "#column" ).append(string)
-  });  
+    if(appendTr) {
+      appendTr()
+    }
+  }); 
+}
+{/* <td>${country[1].file1}</td>\n */}
+function appendCountry (country) {
+  $( "#column" ).append( `
+  <td>${country}</td>\n
+`)
+}
+function appendTr () {
+  $( "#column" ).append( `
+  <tr></tr>
+  `)
 }
